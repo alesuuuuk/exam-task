0a7877f9d3c65402b9f0311917b8f6149dcdeab9
alesuuuuk <114850560+alesuuuuk@users.noreply.github.com>
2023-10-11 20:19:27 +0300

    commit before tasty dinner ;)))) yami yami

--- scripts/index.js
+++ scripts/index.js
@@ -1,24 +1,29 @@
 // functions
-const userDataToLocalStorage=  (phone, email, password) =>{
+// setting user data to ls
+const userDataToLocalStorage = (phone, email, password) => {
+    // creating data frame
     let temp_user = {
         phone, email, password
     };
-    localStorage.setItem("user_data", temp_user)
+    // setting to LS
+    localStorage.setItem("user_data", JSON.stringify(temp_user));
 };
 
+// opening home page 
+const toNextPage=  ()=>{
+    window.open('');
+};
 
 // start point
 document.addEventListener("DOMContentLoaded", () => {
-    localStorage.clear
     const USER_LOGIN_FORM = document.querySelector(".form");
     USER_LOGIN_FORM.addEventListener("submit", (e) => {
         e.preventDefault()
         // getting user data from field
         const USER_PHONE_NUMBER = document.querySelector("#user_phone_number").value; // user phone number
         const USER_EMAIL = document.querySelector("#user_email").value; // user email
-        const USER_PASSWORD = document.querySelector("#user_password").value; // user password
-        console.log(USER_EMAIL)   
+        const USER_PASSWORD = document.querySelector("#user_password").value; // user password   
         userDataToLocalStorage(USER_PHONE_NUMBER, USER_EMAIL, USER_PASSWORD); // here we're creating data frame and sending it to LS
-        console.log(localStorage)
+        
     });
 });
