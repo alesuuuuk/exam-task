033ed72c97446369d7bd8bfaf866a9e9e8293099
school computer <aslesyk@gmail.com>
2023-10-13 17:06:15 +0300

    maybe llogin page got finished

--- scripts/index.js
+++ scripts/index.js
@@ -11,7 +11,7 @@ const userDataToLocalStorage = (phone, email, password) => {
 
 // opening home page 
 const toNextPage = () => {
-    window.open('');
+    window.open('/pages/home.html', '_self');
 };
 
 const showPassword = (selector) => {
@@ -20,10 +20,10 @@ const showPassword = (selector) => {
 
     if (passwordInput && passwordInput.type === "password") {
         passwordInput.type = "text";
-        passwordIcon.setAttribute("name", "unlock");
+        passwordIcon.setAttribute("name", "lock-open-outline");
     } else {
         passwordInput.type = "password";
-        passwordIcon.setAttribute("name", "lock");
+        passwordIcon.setAttribute("name", "lock-closed-outline");
     }
 };
 
@@ -32,19 +32,26 @@ document.addEventListener("DOMContentLoaded", () => {
     // getting elements from DOM
     const USER_LOGIN_FORM = document.querySelector(".form");
     const PASSWORD_ICON = document.querySelector("#password_img");
+    PASSWORD_ICON.addEventListener("click", ()=>{
+        let passwordIconName = PASSWORD_ICON.getAttribute("name")
+        PASSWORD_ICON.setAttribute("name", "airplane-outline")
+    });
     
     
     // adding listeners
     USER_LOGIN_FORM.addEventListener("submit", (e) => {
-        e.preventDefault()
+        e.preventDefault();
         // getting user data from field
         const USER_PHONE_NUMBER = document.querySelector("#user_phone_number").value; // user phone number
         const USER_EMAIL = document.querySelector("#user_email").value; // user email
         const USER_PASSWORD = document.querySelector("#user_password").value; // user password   
         userDataToLocalStorage(USER_PHONE_NUMBER, USER_EMAIL, USER_PASSWORD); // here we're creating data frame and sending it to LS
+        toNextPage();
 
     });
     PASSWORD_ICON.addEventListener("click", () => {
         showPassword("#user_password");
     });
 });
+
+
