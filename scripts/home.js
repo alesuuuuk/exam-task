b6666c81528cdec3232c0bf07eb92c25b4a8e882
alesuuuuk <114850560+alesuuuuk@users.noreply.github.com>
2023-10-22 01:49:23 +0300

    coomit trying to listen a submit(fucking hell)

--- scripts/home.js
+++ scripts/home.js
@@ -1,28 +1,52 @@
 // getting data from LS
-const getUserDataFromLocalStorage=  ()=>{
+const getUserDataFromLocalStorage = () => {
     const USER_DATA = localStorage.getItem("user_data");
-    JSON.parse(USER_DATA);
-    return USER_DATA;
+    
+
+    if (USER_DATA) {
+        try {
+            const userDataObject = JSON.parse(USER_DATA);
+            return userDataObject;
+        } catch (error) {
+            console.error("Error parsing JSON:", error);
+            return null; // Return null or handle the error in an appropriate way
+        }
+    }
+    
+    return null; // Handle the case where "user_data" is not in local storage
 };
 
 // showing burger menu(calling in DOM by onclick)
 const showAndHideBurgerMenu = (element) => {
     const burgerMenu = document.querySelector('.burger__menu');
     if (burgerMenu.style.display === 'flex') {
-      burgerMenu.style.display = 'none';
+        burgerMenu.style.display = 'none';
     } else {
-      burgerMenu.style.display = 'flex';
+        burgerMenu.style.display = 'flex';
     };
-  };
-  
+};
 
+const showUserName = (selector, name) => {
+    let html = `${name}`
+    const AREA = document.querySelector(selector)
+    AREA.innerHTML = html
+   
+}
 
 // start point
-document.addEventListener("DOMContentLoaded", ()=>{
+document.addEventListener("DOMContentLoaded", () => {
     const USER_DATA = getUserDataFromLocalStorage(); // data got
     // getting elements from DOM
-    // const BURGER_BTN=  document.querySelector(".burger__menu")
-    // const BURGER_BTN_CLOSE = document.querySelector(".")
+    showUserName('#user_name', USER_DATA.name)
+    const BOOK_ROOM_FORM=  document.querySelector(".book__room_box_form")
+    BOOK_ROOM_FORM.addEventListener("submit", (e)=>{
+        e.preventDefault
+        alert("tets")
+    })
     
     
+    
+
+
+
 })
--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,9 @@
 // functions
 // setting user data to ls
-const userDataToLocalStorage = (phone, email, password) => {
+const userDataToLocalStorage = (name, email, password) => {
     // creating data frame
-    let temp_user = {
-        phone, email, password
+    const temp_user = {
+        name, email, password
     };
     // setting to LS
     localStorage.setItem("user_data", JSON.stringify(temp_user));
@@ -37,10 +37,10 @@ document.addEventListener("DOMContentLoaded", () => {
     USER_LOGIN_FORM.addEventListener("submit", (e) => {
         e.preventDefault();
         // getting user data from field
-        const USER_PHONE_NUMBER = document.querySelector("#user_phone_number").value; // user phone number
+        const USER_NAME = document.querySelector("#user_name").value; // user phone number
         const USER_EMAIL = document.querySelector("#user_email").value; // user email
         const USER_PASSWORD = document.querySelector("#user_password").value; // user password   
-        userDataToLocalStorage(USER_PHONE_NUMBER, USER_EMAIL, USER_PASSWORD); // here we're creating data frame and sending it to LS
+        userDataToLocalStorage(USER_NAME, USER_EMAIL, USER_PASSWORD); // here we're creating data frame and sending it to LS
         toNextPage(); // opening home page 
 
     });
